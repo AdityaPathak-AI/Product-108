b2d92561472a8c3372771d20212e8da337fd09ac
Aditya Pathak <sumitpathak861@gmail.com>
2023-12-15 22:34:30 +0530

    changes...

--- product-108/src/App.js
+++ product-108/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 
-function App() {
+const App = () => {
   const name = "Aditya";
   const age = 21;
   const city = <h1>Indore</h1>;
+  const state = "Madhya Pradesh";
+  
   return (
     <div className="App">
       <h1> {name} </h1>
       <h1> {age} </h1>
           {city}
+      <h1>{state}</h1>
     </div>
   );
 }
